5ac8ddc2fd539925f377cafb4e09f52736e28a88
judachury <judachury@hotmail.com>
2014-12-31 15:50:25 +0000

    made all the possible maze locations [X,Y]

--- prolog/maze-solver.pl
+++ prolog/maze-solver.pl
@@ -1,3 +1,4 @@
+mazeSize(5,9).
 barrier(1,8).
 barrier(2,8).
 barrier(2,2).
@@ -26,3 +27,32 @@ solve(From, To, Path) :-
 	write('*'),
 	findAWayOut(From,To,Path),
 	write('@@@@').
+	
+% TEST SOME CONCEPTS HERE
+findPath(X,X,[X]).
+findPath(X,X2,[X|R]) :-
+		X < X2, Z is X+1, findPath(Z,X2,R).
+		
+
+findPathSecond([X1|[X2|_]],[Y1|[Y2|_]],R) :-
+	findPath(X1,Y2,R1), 
+	findPath(X2,Y2,R2),	
+	append([R1],[R2],R).
+
+
+makePairsX(0,Y,[]).
+makePairsX(X,Y,R) :-
+	A is X-1, makePairsX(A,Y,Z), append(Z,[[X,Y]],R).
+	
+check(X,L) :-
+	barrier(X,L), write('Yes - '), write([X,L]), nl.
+	
+makePairsY(X,0,[]).
+makePairsY(X,Y,R) :-
+	A is Y-1, makePairsY(X,A,Z), append(Z,[[X,Y]],R).
+
+makeTree(0,Y,R).
+makeTree(X,Y,R) :-
+	makePairsY(X,Y,Z), write(Z), A is X-1, makeTree(A,Y,B), append(B,Z,R).
+
+	
